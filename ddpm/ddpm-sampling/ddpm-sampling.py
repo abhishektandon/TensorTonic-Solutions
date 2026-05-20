650696ad4492271ed1223903a4fc741cf3340439
Abhishek Tandon <115216453+abhishektandon@users.noreply.github.com>
2026-05-20 10:22:59 +0530

    Add solution for ddpm-sampling

---
+++ ddpm/ddpm-sampling/ddpm-sampling.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def ddpm_sample(x_T, betas, epsilon_preds, z_values):
+    """
+    Returns: np.ndarray of the final denoised sample
+    """
+    
+    epsilon_preds = np.array(epsilon_preds)
+    z_values = np.array(z_values)
+    betas = np.array(betas)
+    
+    alphas = 1 - betas
+    alpha_bars = np.cumprod(alphas)
+    T = len(betas)
+    x_t = np.array(x_T)
+    
+    for t in range(T-1, -1, -1):
+        print(t)
+        if t > 0:
+            z = z_values[T-t-1]
+        else:
+            z = np.zeros_like(x_t)
+        
+        # x_t_minus_1 = (x_t - ((1 - alphas[t])/(np.sqrt(1 - alpha_bars[t]))) * epsilon_preds[t]) / (np.sqrt(alphas[t])) + np.sqrt(betas[t]) * z
+        one_by_sq_alpha_t = 1/np.sqrt(alphas[t])
+        one_minus_alpha_t = 1 - alphas[t]
+        sq_one_minus_alpha_t_bar = np.sqrt(1 - alpha_bars[t])
+        
+        x_t_minus_1 = one_by_sq_alpha_t * (x_t - one_minus_alpha_t/sq_one_minus_alpha_t_bar * epsilon_preds[T-t-1]) + np.sqrt(betas[t]) * z
+        
+        x_t = x_t_minus_1
+
+    return x_t
