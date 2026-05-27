53ac0f3e077fc111e80c74657b61e02e3074f453
Abhishek Tandon <115216453+abhishektandon@users.noreply.github.com>
2026-05-27 10:35:20 +0530

    Update solution for transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -7,7 +7,7 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     Compute scaled dot-product attention.
     """
     
-    dim = K.shape[-1]
+    dim = Q.shape[-1]
     atten = (Q @ torch.transpose(K, 1, 2))/math.sqrt(dim)
     softmax_atten = F.softmax(atten, dim=-1) # across the rows 
     out = softmax_atten @ V
