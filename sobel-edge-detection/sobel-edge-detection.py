6630dd9de1163e78092e71c60737d139b6ffb479
Abhishek Tandon <115216453+abhishektandon@users.noreply.github.com>
2026-07-08 10:16:28 +0530

    Solve sobel-edge-detection

---
+++ sobel-edge-detection/sobel-edge-detection.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+
+def sobel_edges(image):
+    """
+    Apply the Sobel operator to detect edges.
+    """
+    pad_width = 1
+    image = np.pad(image, pad_width=pad_width, mode='constant')
+    k1 = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    k2 = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    m, n = image.shape
+    ksize, _ = k1.shape
+
+    m_out = m - ksize + 1
+    n_out = n - ksize + 1
+    
+    gx = np.zeros((m_out, n_out))
+    gy = np.zeros((m_out, n_out))
+    
+    for i in range(m_out):
+        for j in range(n_out):
+            roi = image[i:i+ksize, j:j+ksize]
+            gx[i, j] = np.sum(roi * k1)
+            gy[i, j] = np.sum(roi * k2)
+
+    g = np.sqrt(gx**2 + gy**2)
+    
+    return g.tolist()
