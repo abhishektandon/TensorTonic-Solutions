ca657f72d45947a1ce104c75fe2069da75e05306
Abhishek Tandon <115216453+abhishektandon@users.noreply.github.com>
2026-07-08 15:24:44 +0530

    Solve average-pooling-2d

---
+++ average-pooling-2d/average-pooling-2d.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import math 
+
+def average_pooling_2d(X, pool_size):
+    """
+    Apply 2D avg pooling with non-overlapping windows.
+    """
+    # Write code here
+    image = np.array(X)
+    m, n = image.shape
+    m_out, n_out = m//pool_size, n//pool_size
+
+    output = np.zeros((m_out, n_out))
+    for i in range(m_out):
+        for j in range(n_out):
+            roi = image[i*pool_size:i*pool_size+pool_size, 
+                        j*pool_size:j*pool_size+pool_size]
+            output[i, j] = np.mean(roi)
+
+    return output.tolist()
+    
