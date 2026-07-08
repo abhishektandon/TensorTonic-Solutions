e25aa633145f5797b0b001e7f6a088bbf556f751
Abhishek Tandon <115216453+abhishektandon@users.noreply.github.com>
2026-07-08 14:05:11 +0530

    Solve color-to-grayscale

---
+++ color-to-grayscale/color-to-grayscale.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def color_to_grayscale(image):
+    """
+    Convert an RGB image to grayscale using luminance weights.
+    """
+    image = np.array(image)
+    r = image[:, :, 0]
+    g = image[:, :, 1]
+    b = image[:, :, 2]
+
+    gray = 0.299 * r + 0.587 * g + 0.114 * b 
+
+    return gray.tolist()
